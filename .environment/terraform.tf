0bc56ef18af4f81e9899989b798b3c44244b0e47
clademirjr <clademir@parseint.com.br>
2025-06-14 16:37:20 +0000

    versao terraform

--- .environment/terraform.tf
+++ .environment/terraform.tf
@@ -3,12 +3,12 @@ terraform {
     aws = { source = "hashicorp/aws", version = "~> 5.98" }
   }
   backend "s3" {
-    bucket  = var.BUCKET_S3
+    bucket  = var.BUCKET_S3           # Variável definida em terraform.tfvars
     key     = "state/terraform.tfstate"
-    region  = "us-east-1"  # region must be static or provided via -backend-config
+    region  = "us-east-1"            # Região estática ou via -backend-config
     encrypt = true
   }
-  required_version = ">= 1.12.1"
+  required_version = ">= 1.4.0"  # Ajustado para suportar Terraform 1.4.0 do runner
 }
 
 provider "aws" {
