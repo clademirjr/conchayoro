4b2d8b1d08b86d118ee2215f58a1274874ca771a
clademirjr <clademir@parseint.com.br>
2025-06-14 17:12:30 +0000

    teste

--- .environment/terraform.tf
+++ .environment/terraform.tf
@@ -1,14 +1,19 @@
 terraform {
   required_providers {
-    aws = { source = "hashicorp/aws", version = "~> 5.98" }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.98"
+    }
   }
-  backend "s3" {
-    bucket  = var.BUCKET_S3           # Variável definida em terraform.tfvars
-    key     = "state/terraform.tfstate"
-    region  = "us-east-1"            # Região estática ou via -backend-config
-    encrypt = true
-  }
-  required_version = ">= 1.4.0"  # Ajustado para suportar Terraform 1.4.0 do runner
+
+  # backend "s3" {
+  #   bucket  = var.BUCKET_S3
+  #   key     = "state/terraform.tfstate"
+  #   region  = "us-east-1"
+  #   encrypt = true
+  # }
+
+  required_version = ">= 1.4.0"
 }
 
 provider "aws" {
